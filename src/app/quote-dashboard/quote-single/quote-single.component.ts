d9e65216b9fee1dabe3e273c052a79674140c722
Kashaer <kaiser.kaiser211@gmail.com>
2018-02-20 21:41:22 -0500

    initial commit

---
+++ src/app/quote-dashboard/quote-single/quote-single.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Quote } from '../../quote';
+
+@Component({
+  selector: 'app-quote-single',
+  templateUrl: './quote-single.component.html',
+  styleUrls: ['./quote-single.component.css']
+})
+export class QuoteSingleComponent implements OnInit {
+
+  @Input() quote: Quote;
+
+  @Input() index: number;
+
+  @Output() destroyQuoteEvent = new EventEmitter();
+
+  constructor() { }
+  ngOnInit() {
+  	// console.log(this.quote);
+  	console.log(this.index);
+  }
+  increaseVote(){
+  	// console.log('It works');
+  	this.quote.votes++;
+
+  }
+  decreaseVote(){
+  	if(this.quote.votes < 2){
+  		this.quote.votes = 0;
+  	} else {
+  		this.quote.votes--;
+  	}
+  	
+  }
+  destroyQuote(){
+  	// set off an event emitter
+  	this.destroyQuoteEvent.emit(this.index);
+  }
+}
+
+
+
+
+
+
+
+
